002893cb870b26db3e6e7c7dce17ab58b93187b6
Francesco Pasqua <cesconix@me.com>
2022-08-05 15:53:35 +0200

    minor changes

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -8,6 +9,10 @@ class MyDocument extends Document {
         <body className="loading">
           <Main />
           <NextScript />
+          <Script
+            src="https://cdn.html.games.alexa.a2z.com/alexa-html/latest/alexa-html.js"
+            strategy="beforeInteractive"
+          ></Script>
         </body>
       </Html>
     )
