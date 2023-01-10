6c1586e880617014a657a234db8422c08ec90094
Francesco Pasqua <cesconix@me.com>
2023-01-11 00:28:31 +0100

    feat(kickoff): jakala

--- site/pages/api/twilio.ts
+++ site/pages/api/twilio.ts
@@ -1,10 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import twilio from 'twilio'
+import nodemailer from 'nodemailer'
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
-const sender = process.env.TWILIO_SENDER
-const recipients = process.env.TWILIO_RECIPIENTS
+const smsSender = process.env.TWILIO_SENDER
+const smsRecipients = process.env.TWILIO_RECIPIENTS
+const emailSender = process.env.EMAIL_SENDER
+const emailSenderPass = process.env.EMAIL_SENDER_PASS
+const emailRecipients = process.env.EMAIL_RECIPIENTS
 
 const twilioClient = twilio(accountSid, authToken)
 
@@ -16,21 +20,42 @@ async function twilioApi(req: NextApiRequest, res: NextApiResponse) {
   }
 
   try {
-    const allPromises = []
-
-    for (const recipient of recipients?.split(',')!) {
+    const allPromisesForSms = []
+    for (const smsRecipient of smsRecipients?.split(',')!) {
       const promise = twilioClient.messages.create({
-        from: `${sender}`,
+        from: `${smsSender}`,
         // body: "Siamo su un NFT insieme! 🚀\n\nDai un'occhiata su OpenSea per vederci in azione durante lo spettacolo di Enabling Solutions.\n\nhttps://opensea.io/collection/hesn\n\nIl tuo collega,\nFrancesco Pasqua",
         body: message,
-        to: `${recipient}`,
+        to: `${smsRecipient}`,
       })
 
-      allPromises.push(promise)
+      allPromisesForSms.push(promise)
     }
+    const psForSms = await Promise.allSettled(allPromisesForSms)
+    console.log(psForSms)
 
-    const ps = await Promise.allSettled(allPromises)
-    console.log(ps)
+    const allPromisesForEmail = []
+    for (const emailRecipient of emailRecipients?.split(',')!) {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: emailSender,
+          pass: emailSenderPass,
+        },
+      })
+
+      const promise = transporter.sendMail({
+        from: '"Francesco Pasqua" <francesco.pasqua@h-farm.com>',
+        to: emailRecipient,
+        subject: 'NFT from Nakamoto Program',
+        text: message,
+        html: message.split('\n').join('<br>') + '<br>',
+      })
+
+      allPromisesForEmail.push(promise)
+    }
+    const psForEmail = await Promise.allSettled(allPromisesForEmail)
+    console.log(psForEmail)
 
     return res.status(200).send({})
   } catch (error: any) {
